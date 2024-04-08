4bfe57abf3649afdabce8756d1c90c2cf9cdb1f4
DouglasTakada <douglas.takada@gmail.com>
2024-04-07 23:29:52 -0700

    Completed hw_4

--- CPT_S_427/HW4/hw4.py
+++ CPT_S_427/HW4/hw4.py
@@ -1,35 +1,25 @@
 import passlib as pl
 from datetime import datetime
 
-# Open a file named "demofile.txt" for reading
+# Open a file named "Password.txt" for reading
 pw = open("Password.txt", "r")
-# Open a file named "demofile.txt" for reading
+# Open a file named "wordList.txt" for reading
 wl = open("wordList.txt", "r")
 
-pw_l= []
-
+pw_l= [] 
 # put password data into datastructure for easy sorting and manipulation
 for line in pw:
     pw_l.append(line.strip().split(','))
 
-# sorted_login_list = sorted(pw_l, key=lambda x: int(x[-1]), reverse=True)
-# print(sorted_login_list[:10])
+# Problem #2
+sorted_login_list = sorted(pw_l, key=lambda x: int(x[-1]), reverse=False)
+for i in range(1,11):
+    print(sorted_login_list[-i][2]+":"+sorted_login_list[-i][-1])
 
+# Problem #3
 sorted_time_list = sorted(pw_l, key=lambda x: datetime.strptime(x[-2], "%Y%m%dT%H%M%SZ"), reverse=True)
-# print(sorted_time_list[:10])
-
-print(sorted_time_list[-1][2],datetime.strptime(sorted_time_list[-1][-2], "%Y%m%dT%H%M%SZ"))
-print(datetime.strptime(sorted_time_list[-2][-2], "%Y%m%dT%H%M%SZ"))
-print(datetime.strptime(sorted_time_list[-3][-2], "%Y%m%dT%H%M%SZ"))
-print(datetime.strptime(sorted_time_list[-4][-2], "%Y%m%dT%H%M%SZ"))
-print(datetime.strptime(sorted_time_list[-5][-2], "%Y%m%dT%H%M%SZ"))
-
-# print(datetime.strptime(sorted_time_list[1][-2], "%Y%m%dT%H%M%SZ"))
-# print(datetime.strptime(sorted_time_list[2][-2], "%Y%m%dT%H%M%SZ"))
-# print(datetime.strptime(sorted_time_list[3][-2], "%Y%m%dT%H%M%SZ"))
-# print(datetime.strptime(sorted_time_list[4][-2], "%Y%m%dT%H%M%SZ"))
-# print(datetime.strptime(sorted_time_list[5][-2], "%Y%m%dT%H%M%SZ"))
-
+for i in range(1,11):
+    print(sorted_time_list[-i][2]+":"+str(datetime.strptime(sorted_time_list[-i][-2], "%Y%m%dT%H%M%SZ")))
 
 # # Open the file in write mode ('w')
 # with open('upatedpw.txt', 'w') as file:
